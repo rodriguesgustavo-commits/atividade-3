bc4f6d28663229ceb1a48ca1fa5949c6b87338a3
Gustavo Rodrigues <rodrigues.gustavo20@escola.pr.gov.br>
2026-04-29 09:40:32 -0300

    Add files via upload

---
+++ atividade3.js
@@ -0,0 +1,97 @@
+// Modelo da lista de objetos para o projeto 2:
+const produtos = [
+    {
+        nome: "Monitor",
+        valor: "R$ 1200,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "AAAAAAA",
+        codigo: 1234
+    },
+    {
+        nome: "Teclado",
+        valor: "R$ 250,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBBBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Mouse",
+        valor: "R$ 150,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "CCCCC",
+        codigo: 1234
+    },
+    {
+        nome: "headset",
+        valor: "R$ 300,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "DDDDDDD",
+        codigo: 1234
+    },
+    {
+        nome: "Cadeira Gamer",
+        valor: "R$ 1500,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "EEEEEE",
+        codigo: 1234
+    },
+    {
+        nome: "Gabinete",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    { {
+        nome: "Memória RAM",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+        nome: "Teclado",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Teclado",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    {
+        nome: "Teclado",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+    { {
+        nome: "Teclado",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+        nome: "Teclado",
+        valor: "R$ 20,00",
+        quantidade: 100,
+        peso: 10,
+        marca: "BBBB",
+        codigo: 1234
+    },
+]
